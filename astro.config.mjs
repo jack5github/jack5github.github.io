fe25d3057ab336c5a0f786c1876fa7cfcf4516dc
Jack5 <jack5answers@gmail.com>
2026-05-31 15:35:53 +1000

    Add REPO article and YouTube embeds

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,4 +11,5 @@ export default defineConfig({
 	redirects: {
 		'/article/permanently-mount-storage-device-linux-guide': '/article/gparted',
 	},
+	integrations: [mdx()],
 });
