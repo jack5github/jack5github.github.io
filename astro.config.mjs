4f8898430e6813670846f147560bf9c05ce32c04
Jack5 <32806494+jack5github@users.noreply.github.com>
2026-06-01 15:54:14 +1000

    Add Pagefind search and footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import pagefind from 'astro-pagefind';
+import { unified } from '@astrojs/markdown-remark';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://jack5github.github.io',
 	markdown: {
-		smartypants: false,
+		processor: unified({ smartypants: false }),
 	},
 	redirects: {
 		'/article/permanently-mount-storage-device-linux-guide': '/article/gparted',
 	},
-	integrations: [mdx()],
+	integrations: [mdx(), pagefind()],
 });
